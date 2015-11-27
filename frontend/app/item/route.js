c498a7f30254c895a14b1b89ffe9e41bf171c68c
Michal Bryxí <michal.bryxi@gmail.com>
2015-11-27 16:03:44 +0100

    Item cleanup.

--- frontend/app/item/route.js
+++ frontend/app/item/route.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model () {
+    return [
+      {
+        title: 'foo',
+        body: 'bar',
+        image: 'https://scontent-ams3-1.xx.fbcdn.net/hphotos-xtp1/t31.0-8/12273601_10207990293546059_9151703523103933532_o.jpg'
+      }
+    ];
+  }
 });
